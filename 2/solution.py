21880c285c491cbd1c7d76e53a5fb9dc53e3aab2
sweemeng <sweester@gmail.com>
2020-12-02 17:47:16 +0800

    Refactor and clean

--- 2/solution.py
+++ 2/solution.py
@@ -1,43 +1,9 @@
 def process(input_str):
-    min_count = 0
-    max_count = 0
-    count = 0
-    check = ""
-    buffer = ""
-    data_mode = False
-    for item in input_str:
-        if item == "-":
-            min_count = int(buffer)
-            buffer = ""
-        elif item == " ":
-            if not data_mode:
-                max_count = int(buffer)
-                buffer = ""
-        elif item == ":":
-            data_mode = True
-        else:
-            if not data_mode:
-                check = item
-                buffer = buffer + item
-            else:
-                if item == check:
-                    count = count + 1
-
-    if count >= min_count and count <= max_count:
-        return True
-    return False
-
-
-def process2(input_str):
     first = 0
     second = 0
-    count = 0
     ptr = 0
     check = ""
     buff = ""
-    valid = False
-    check1 = False
-    check2 = False
     data_mode = False
     for item in input_str:
         if item == "-":
@@ -55,29 +21,29 @@ def process2(input_str):
                 buff = buff + item
             else:
                 ptr = ptr + 1
-                if ptr == first:
-                    check1 = item == check
-                if ptr == second:
-                    check2 = item == check
-    if check1 != check2:
-        return True
-    return False
+                yield first, second, check, ptr, item
 
 def main():
-    f = open("input")
-    l = f.readlines()
-    count = 0
-    for i in l:
-        if process(i):
-            count = count + 1
-    
-    print("first count:", count)
-    count = 0
-    for i in l:
-        if process2(i):
-            count = count + 1
-    print("second count:", count)
-
+    with open("input") as f:
+        password_count_1 = 0
+        password_count_2 = 0
+        for i in f:
+            char_count = 0
+            check_1 = False
+            check_2 = False
+            for j in process(i):
+                first, second, check, ptr, item = j
+                if item == check:
+                    char_count = char_count + 1
+                if ptr == first:
+                    check_1 = check == item
+                if ptr == second:
+                    check_2 = check == item
+            if char_count >= first and char_count <= second:
+                password_count_1 = password_count_1 + 1
+            if check_1 != check_2:
+                password_count_2 = password_count_2 + 1
+        print(f"first count: {password_count_1}, second count: {password_count_2}")
 
 if __name__ == "__main__":
     main()
