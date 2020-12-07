9db2ac957e64dbf2910f50298f7c3fcbfd264bf7
sweemeng <sweester@gmail.com>
2020-12-07 16:01:03 +0800

    Make test clearer

--- 7/test_solution.py
+++ 7/test_solution.py
@@ -39,6 +39,25 @@ full_rule_trees = {
 }
 
 
+rules_str_1 = """light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags."""
+
+
+rules_str_2 = """shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags."""
+
 
 @pytest.mark.parametrize("rule,expected",test_rules)
 def test_parse_sentence(rule, expected):
@@ -68,16 +87,13 @@ def test_check_gold(color, rules, expected):
     assert exist == expected
     
 
-def test_total_child_bag():
-    rules = """shiny gold bags contain 2 dark red bags.
-dark red bags contain 2 dark orange bags.
-dark orange bags contain 2 dark yellow bags.
-dark yellow bags contain 2 dark green bags.
-dark green bags contain 2 dark blue bags.
-dark blue bags contain 2 dark violet bags.
-dark violet bags contain no other bags."""
+@pytest.mark.parametrize("rules,expected", [
+        (rules_str_1, 32),
+        (rules_str_2, 126),
+])
+def test_total_child_bag(rules, expected):
     rule_tree = solution.make_rule_tree(rules.split("\n"))
 
     total_child = solution.count_child("shiny gold", rule_tree)
-    assert total_child == 126
+    assert total_child == expected
 
